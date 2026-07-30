77e5f979a0d2eb34321b2af92fc60cd3dc375200
lorenzo gennari <gennari.lorenzo05@gmail.com>
2026-07-30 19:02:18 +0200

    navbar v3

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './utils/i18n';
 
 import { AmbientBackground } from './components/layout/AmbientBackground';
-import { LockNavbar } from './components/layout/LockNavbar';
+import { InstagramGlassNavbar } from './components/layout/InstagramNavbar';
 import { HeaderOverlay } from './components/layout/HeaderOverlay';
 
 import { HomePage } from './pages/HomePage';
@@ -19,7 +19,7 @@ export const App: React.FC = () => {
         <AmbientBackground />
 
         {/* Navbar con Lucchetto Interattivo & Overlay Lingua */}
-        <LockNavbar />
+        <InstagramGlassNavbar />
         <HeaderOverlay />
 
         {/* Rotte Multi-Pagina */}
